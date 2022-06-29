1bed53f947ddb4a6034b5795a56f7b0d96ec2c4d
Rafał Piechna <rpiechna@paradyz.com.pl>
2022-06-29 21:37:17 +0200

    pobieranie rozmiaru plikow

--- ZadanieIV/zadanieIV.py
+++ ZadanieIV/zadanieIV.py
@@ -2,4 +2,5 @@ import os
 
 nazwa_pierwszego_pliku = input('prosze wpisac sciezke do pierwszego pliku: ')
 nazwa_drugiego_pliku =  input('prosze wpisac sciezke do drugiego pliku: ')
-
+rozmiar_pierwszego_pliku=os.path.getsize(nazwa_pierwszego_pliku)
+rozmiar_drugiego_pliku=os.path.getsize(nazwa_drugiego_pliku)
